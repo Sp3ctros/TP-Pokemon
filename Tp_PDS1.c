5c336cf976b76b166a39075ad60d11eb428f6a4a
JoaovcLisboa <90469235+Sp3ctros@users.noreply.github.com>
2024-06-11 20:27:30 -0300

    Update Tp_PDS1.c

--- Tp_PDS1.c
+++ Tp_PDS1.c
@@ -7,11 +7,22 @@
 #define Bonus_ataque 1.2f
 #define Debuff_ataque 0.8f
 
-
-
+// Cores ANSI
+#define RESET "\033[0m"
+#define FOGO_COR "\033[31m"
+#define VERDE_COR "\033[32m"
+#define ELETRICO_COR "\033[33m"
+#define AGUA_COR "\033[34m"
+#define ROXO "\033[35m"
+#define GELO_COR "\033[36m"
+#define BRANCO_COR "\033[37m"
+#define PEDRA_COR "\033[090m"
+#define BALD "\033[1m"
 
 // tipos de pokemons como constantes para o calculo de batalha
 enum TipoPokemon { ELETRICO = 1, AGUA = 2, FOGO = 3, GELO = 4, PEDRA = 5};
+
+
 
 // Estrutura de um Pokémon
 struct pokemon
@@ -21,6 +32,7 @@ struct pokemon
     int ataque;
     int defesa;
     float vida;
+    char cor[MAX_STRING_LEN];
 };
 
 // EStrutura de um treinador
@@ -31,7 +43,120 @@ typedef struct
     int num_pokemons_vivos;
 } treinador;
 
+//escolhe a cor do pokemon baseado no tipo
+void escolher_cor(char* cor, const char* tipo) {
+    switch (tolower(tipo[0])) 
+    {
+        case 'a':
+            strcpy(cor, AGUA_COR);
+            break;
+        case 'e':
+            strcpy(cor, ELETRICO_COR);
+            break;
+        case 'f':
+            strcpy(cor, FOGO_COR);
+            break;
+        case 'g':
+            strcpy(cor, GELO_COR);
+            break;
+        case 'p':
+            strcpy(cor, PEDRA_COR);
+            break;
+        default:
+            strcpy(cor, RESET);
+            break;
+    }
+}
 
+
+// Função para ler pokémons de um arquivo e separar no vetor treinador A e B
+int lerPokemons(FILE *arq, treinador *treinadorA, treinador *treinadorB) 
+{
+    char linha_buffer[MAX_STRING_LEN];
+    fscanf(arq, "%d %d\n", &treinadorA->num_pokemons, &treinadorB->num_pokemons); // Pega o numero de pokemons de cada treinador
+    if (treinadorA->num_pokemons < 1 || treinadorB->num_pokemons < 1)
+    {
+        printf("Num pokemons:\nTreinador A: %d\nTreinador B: %d\n", treinadorA->num_pokemons, treinadorB->num_pokemons);
+        return 1;
+    }
+    else if (treinadorA->num_pokemons > 100 || treinadorB->num_pokemons > 100)
+    {
+        printf("Num pokemons:\nTreinador A: %d\nTreinador B: %d\n", treinadorA->num_pokemons, treinadorB->num_pokemons);
+        return 2;
+    }
+    treinadorA->num_pokemons_vivos = treinadorA->num_pokemons;
+    treinadorB->num_pokemons_vivos = treinadorB->num_pokemons;
+    for (int i = 0; i < treinadorA->num_pokemons + treinadorB->num_pokemons; i++) // separa os dados de cada pokemon
+    {
+        fgets(linha_buffer, MAX_STRING_LEN, arq);
+        if (i < treinadorA->num_pokemons) 
+        { 
+            //separa os dados do pokemon e verifica se ele tem 5 caracteristicas.
+            if (sscanf(linha_buffer, "%s %d %d %f %s", treinadorA->cartel_pokemon[i].nome, &treinadorA->cartel_pokemon[i].ataque, &treinadorA->cartel_pokemon[i].defesa, &treinadorA->cartel_pokemon[i].vida, treinadorA->cartel_pokemon[i].tipo) != 5)
+            {
+                printf("%s", linha_buffer);
+                return 3;
+            }
+            escolher_cor(treinadorA->cartel_pokemon[i].cor, treinadorA->cartel_pokemon[i].tipo);
+        }
+        else 
+        {
+            if (sscanf(linha_buffer, "%s %d %d %f %s", treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].nome, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].ataque, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].defesa, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].vida, treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].tipo) != 5)
+            {
+                printf("%s", linha_buffer);
+                return 3;
+            }
+            escolher_cor(treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].cor, treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].tipo);
+
+        }
+    }
+    return 0;
+}
+
+
+
+void mostrar_dados(treinador *treinadorA, treinador *treinadorB)
+{
+    printf(BALD"------------------------------------------------ DADOS DOS POKEMONS -----------------------------------------------------------------------------------------------------------\n");
+    printf("%40s %60s\n", "Treinador A","Treinador B");
+    printf("%40s:%4d %60s:%4d\n", "Numeros de pokemons", treinadorA->num_pokemons, "Numeros de pokemons",treinadorB->num_pokemons);
+    printf("%-20s %-6s %-6s %-6s %38s %-20s %-6s %-6s %-6s\n", 
+           "Nome", "Ataque", "Defesa", "Vida", "", "Nome", "Ataque", "Defesa", "Vida"RESET);
+
+    int max_pokemons = (treinadorA->num_pokemons > treinadorB->num_pokemons) ? treinadorA->num_pokemons  : treinadorB->num_pokemons;
+
+    for (int i = 0; i < max_pokemons; i++)
+    {
+        if (i < treinadorA->num_pokemons)
+        {
+            printf("%s%-20s %-6d %-6d %-6.0f"RESET,
+                   treinadorA->cartel_pokemon[i].cor,
+                   treinadorA->cartel_pokemon[i].nome,
+                   treinadorA->cartel_pokemon[i].ataque, 
+                   treinadorA->cartel_pokemon[i].defesa,
+                   treinadorA->cartel_pokemon[i].vida);
+        }
+        else
+        {
+            printf("%-20s %-6s %-6s %-6s", "", "", "", "");
+        }
+
+        printf("%40s", "");
+
+        if (i < treinadorB->num_pokemons)
+        {
+            printf("%s%-20s %-6d %-6d %-6.0f"RESET, 
+                   treinadorB->cartel_pokemon[i].cor,
+                   treinadorB->cartel_pokemon[i].nome, 
+                   treinadorB->cartel_pokemon[i].ataque, 
+                   treinadorB->cartel_pokemon[i].defesa,
+                   treinadorB->cartel_pokemon[i].vida);
+        }
+
+        printf("\n");
+    }
+    printf(BALD"------------------------------------------------------------------------------------------------------------------------------------------------------------\n"RESET);
+}
 
 // converter o tipo de pokemon para valor numérico
 int converterTipo(char*tipo)
@@ -95,7 +220,7 @@ int pokebatalha(treinador *atacante, int index_A, treinador *defensor, int index
     if (defensor->cartel_pokemon[index_B].vida <= 0)
     {
         defensor->num_pokemons_vivos--;
-        printf("%s venceu %s\n", atacante->cartel_pokemon[index_A].nome, defensor->cartel_pokemon[index_B].nome);
+        printf("%s" "%s" RESET VERDE_COR " venceu " RESET"%s" "%s\n" RESET, atacante->cartel_pokemon[index_A].cor, atacante->cartel_pokemon[index_A].nome, defensor->cartel_pokemon[index_B].cor, defensor->cartel_pokemon[index_B].nome);
         if (defensor->num_pokemons_vivos <= 0)
         {
             return 0;
@@ -108,46 +233,7 @@ int pokebatalha(treinador *atacante, int index_A, treinador *defensor, int index
 }
 
 
-// Função para ler pokémons de um arquivo e separar no vetor treinador A e B
-int lerPokemons(FILE *arq, treinador *treinadorA, treinador *treinadorB) 
-{
-    char linha_buffer[MAX_STRING_LEN];
-    fscanf(arq, "%d %d\n", &treinadorA->num_pokemons, &treinadorB->num_pokemons); // Pega o numero de pokemons de cada treinador
-    if (treinadorA->num_pokemons < 1 || treinadorB->num_pokemons < 1)
-    {
-        printf("Num pokemons:\nTreinador A: %d\nTreinador B: %d\n", treinadorA->num_pokemons, treinadorB->num_pokemons);
-        return 1;
-    }
-    else if (treinadorA->num_pokemons > 100 || treinadorB->num_pokemons > 100)
-    {
-        printf("Num pokemons:\nTreinador A: %d\nTreinador B: %d\n", treinadorA->num_pokemons, treinadorB->num_pokemons);
-        return 2;
-    }
-    treinadorA->num_pokemons_vivos = treinadorA->num_pokemons;
-    treinadorB->num_pokemons_vivos = treinadorB->num_pokemons;
-    for (int i = 0; i < treinadorA->num_pokemons + treinadorB->num_pokemons; i++) // separa os dados de cada pokemon
-    {
-        fgets(linha_buffer, MAX_STRING_LEN, arq);
-        if (i < treinadorA->num_pokemons) 
-        { 
-            //separa os dados do pokemon e verifica se ele tem 5 caracteristicas.
-            if (sscanf(linha_buffer, "%s %d %d %f %s", treinadorA->cartel_pokemon[i].nome, &treinadorA->cartel_pokemon[i].ataque, &treinadorA->cartel_pokemon[i].defesa, &treinadorA->cartel_pokemon[i].vida, treinadorA->cartel_pokemon[i].tipo) != 5)
-            {
-                printf("%s", linha_buffer);
-                return 3;
-            }
-        } 
-        else 
-        {
-            if (sscanf(linha_buffer, "%s %d %d %f %s", treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].nome, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].ataque, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].defesa, &treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].vida, treinadorB->cartel_pokemon[i - treinadorA->num_pokemons].tipo) != 5)
-            {
-                printf("%s", linha_buffer);
-                return 3;
-            }
-        }
-    }
-    return 0;
-}
+
 
 // Mostra todos pokemons que nao foram derrotados
 void mostrar_sobreviventes(treinador *treinadorA, treinador *treinadorB)
@@ -159,14 +245,15 @@ void mostrar_sobreviventes(treinador *treinadorA, treinador *treinadorB)
         {
             if (treinadorA->cartel_pokemon[i].vida > 0)
              {
-                printf("%s\n", treinadorA->cartel_pokemon[i].nome);
+                printf("%s%s\n"RESET, treinadorA->cartel_pokemon[i].cor,treinadorA->cartel_pokemon[i].nome);
+
              }
         }
         else
         {
             if (treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].vida > 0)
             {
-                printf("%s\n", treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].nome);
+                printf("%s%s\n"RESET, treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].cor, treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].nome);
             }
         }
     }
@@ -175,66 +262,27 @@ void mostrar_sobreviventes(treinador *treinadorA, treinador *treinadorB)
 // Mostra todos pokemons que foram derrotados
 void mostrar_derrotados(treinador *treinadorA, treinador *treinadorB)
 {
-    printf("Pokemons derrotados: \n");
+    printf(BALD"\n\nPokemons derrotados: \n"RESET);
     for(int i = 0; i < treinadorA->num_pokemons + treinadorB->num_pokemons; i++)
     {
         if (i < treinadorA->num_pokemons)
         {
             if (treinadorA->cartel_pokemon[i].vida <= 0)
              {
-                printf("%s\n", treinadorA->cartel_pokemon[i].nome);
+                printf("%s%s\n"RESET, treinadorA->cartel_pokemon[i].cor,treinadorA->cartel_pokemon[i].nome);
              }
         }
         else
         {
             if (treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].vida <= 0)
             {
-                printf("%s\n", treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].nome);
+                printf("%s%s\n"RESET, treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].cor, treinadorB->cartel_pokemon[i-treinadorA->num_pokemons].nome);
             }
         }
     }
 }
 
-void mostrar_dados(treinador *treinadorA, treinador *treinadorB)
-{
-    printf("------------------------------------------------ DADOS DOS POKEMONS -----------------------------------------------------------------------------------------------------------\n");
-    printf("%40s %60s\n", "Treinador A","Treinador B");
-    printf("%40s:%4d %60s:%4d\n", "Numeros de pokemons", treinadorA->num_pokemons, "Numeros de pokemons",treinadorB->num_pokemons);
-    printf("%-20s %-6s %-6s %-6s %38s %-20s %-6s %-6s %-6s\n", 
-           "Nome", "Ataque", "Defesa", "Vida", "", "Nome", "Ataque", "Defesa", "Vida");
 
-    int max_pokemons = (treinadorA->num_pokemons > treinadorB->num_pokemons) ? treinadorA->num_pokemons  : treinadorB->num_pokemons;
-
-    for (int i = 0; i < max_pokemons; i++)
-    {
-        if (i < treinadorA->num_pokemons)
-        {
-            printf("%-20s %-6d %-6d %-6.0f", 
-                   treinadorA->cartel_pokemon[i].nome,
-                   treinadorA->cartel_pokemon[i].ataque, 
-                   treinadorA->cartel_pokemon[i].defesa,
-                   treinadorA->cartel_pokemon[i].vida);
-        }
-        else
-        {
-            printf("%-20s %-6s %-6s %-6s", "", "", "", "");
-        }
-
-        printf("%40s", "");
-
-        if (i < treinadorB->num_pokemons)
-        {
-            printf("%-20s %-6d %-6d %-6.0f", 
-                   treinadorB->cartel_pokemon[i].nome, 
-                   treinadorB->cartel_pokemon[i].ataque, 
-                   treinadorB->cartel_pokemon[i].defesa,
-                   treinadorB->cartel_pokemon[i].vida);
-        }
-
-        printf("\n");
-    }
-    printf("------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
-}
 
 
 int main() 
@@ -267,7 +315,7 @@ int main()
     //BATALHAAAA!
     mostrar_dados(&treinadorA, &treinadorB);
     if (pokebatalha(&treinadorA, pok_A_index, &treinadorB, pok_B_index) == 1) return 4;
-    printf(treinadorA.num_pokemons_vivos == 0 ? "Jogador 2 venceu\n": "Jogador 1 venceu\n");
+    printf( treinadorA.num_pokemons_vivos == 0 ? BALD"\n\nJogador 2 venceu\n\n"RESET: BALD"\n\nJogador 1 venceu\n\n"RESET);
     mostrar_sobreviventes(&treinadorA, &treinadorB);
     mostrar_derrotados(&treinadorA, &treinadorB);
     printf("\n");
